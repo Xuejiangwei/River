07f7fa7116aeb5d0b255350deafec780364af915
Xuejiangwei <53032818+Xuejiangwei@users.noreply.github.com>
2024-02-07 22:42:31 +0800

    nvidia driver update solve problem that GetCPUDescriptorHandleForHeapStart() return Ptr is 1

--- River/River/Renderer/DX12Renderer/Source/DX12DynamicDescriptorHeap.cpp
+++ River/River/Renderer/DX12Renderer/Source/DX12DynamicDescriptorHeap.cpp
@@ -2,17 +2,18 @@
 #include "Renderer/DX12Renderer/Header/DX12DynamicDescriptorHeap.h"
 #include "Renderer/DX12Renderer/Header/DX12RHI.h"
 
-#define MAX_DYNAMIC_DESCRIPTOR_NUM 1024
+#define MAX_DYNAMIC_DESCRIPTOR_NUM 65536
 
 DX12DynamicDescriptorHeap::DX12DynamicDescriptorHeap()
 {
-    D3D12_DESCRIPTOR_HEAP_DESC Desc;
-    Desc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
-    Desc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
-    Desc.NumDescriptors = MAX_DYNAMIC_DESCRIPTOR_NUM;
-    Desc.NodeMask = 1;
+    D3D12_DESCRIPTOR_HEAP_DESC heapDesc = {};
+    heapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
+    heapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
+    heapDesc.NumDescriptors = MAX_DYNAMIC_DESCRIPTOR_NUM;
 
-    ThrowIfFailed(((DX12RHI*)RHI::Get().get())->GetDevice()->CreateDescriptorHeap(&Desc, IID_PPV_ARGS(&m_Heap))); 
+    ThrowIfFailed(((DX12RHI*)RHI::Get().get())->GetDevice()->CreateDescriptorHeap(&heapDesc, IID_PPV_ARGS(&m_Heap)));
+
+    //因为Nvidia驱动问题，曾导致m_Heap->GetCPUDescriptorHandleForHeapStart() 返回的Ptr为1
 }
 
 DX12DynamicDescriptorHeap::~DX12DynamicDescriptorHeap()
